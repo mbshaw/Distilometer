8f65541d45250b40878cc73a84734cddb9357df4
Mark Shaw <mark@shawte.com>
2018-11-30 13:13:31 +1300

    Update distilCalcs.js

--- distilCalcs.js
+++ distilCalcs.js
@@ -1,12 +1,12 @@
 
 // dilute a volume from one percentage to another
 function diluteToPpercent(vol,alc1,alc2) {
-  return vol*((alc1/alc2)-1);
+  return vol*((alc1/alc2)-1).toFixed(3);
 }
 
 // make a set volume of a fixed percentage returns an array of alcohol and water
-function volOfPercent(vol,alc1,akc2) {
+function volOfPercent(vol,alc1,alc2) {
   var alcVol = vol*(alc2/alc1);
-  return [alcVOL,(vol-alcVol)];
+  return [alcVol.toFixed(3),(vol-alcVol).toFixed(3)];
 }
 
